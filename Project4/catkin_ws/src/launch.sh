141a9d6a82e4567493c0ec3829460baac304c4a7
szgula <szgula27@gmail.com>
2020-06-24 12:27:14 +0000

    change map

---
+++ Project4/catkin_ws/src/launch.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+xterm -e " roslaunch my_robot world.launch " &
+sleep 5
+xterm -e " roslaunch my_robot mapping.launch " &
+sleep 5
+xterm -e " rosrun teleop_twist_keyboard teleop_twist_keyboard.py "
